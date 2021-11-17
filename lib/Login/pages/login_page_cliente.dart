a9a9980c4d95ad7246d3d4ea2816308edb24d342
DevMatheusMartinez <matheusmartinez.1@hotmail.com>
2021-11-17 16:01:38 -0300

    Projeto Iniciado

---
+++ lib/Login/pages/login_page_cliente.dart
@@ -0,0 +1,185 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:master_revenda/Home/home.dart';
+import 'package:master_revenda/Home/homeRevendedor.dart';
+import 'package:master_revenda/Login/common/theme_helper.dart';
+
+import 'forgot_password_page.dart';
+import 'profile_page.dart';
+import 'registration_page.dart';
+import 'widgets/header_widget.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  double _headerHeight = 250;
+  Key _formKey = GlobalKey<FormState>();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    User? user = FirebaseAuth.instance.currentUser;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: _headerHeight,
+              child: HeaderWidget(_headerHeight, true,
+                  Icons.login_rounded), //let's create a common header widget
+            ),
+            SafeArea(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: EdgeInsets.fromLTRB(
+                      20, 10, 20, 10), // This will be the login form
+                  child: Column(
+                    children: [
+                      Text(
+                        'Master Revenda',
+                        style: TextStyle(
+                            fontSize: 60, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Faça login',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      SizedBox(height: 30.0),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              Container(
+                                child: TextField(
+                                  controller: emailController,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Email', 'digite seu email'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              SizedBox(height: 30.0),
+                              Container(
+                                child: TextField(
+                                  controller: passwordController,
+                                  obscureText: true,
+                                  decoration: ThemeHelper().textInputDecoration(
+                                      'Senha', 'digite sua senha'),
+                                ),
+                                decoration:
+                                    ThemeHelper().inputBoxDecorationShaddow(),
+                              ),
+                              SizedBox(height: 15.0),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              ForgotPasswordPage()),
+                                    );
+                                  },
+                                  child: Text(
+                                    "Esqueceu a senha?",
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                decoration:
+                                    ThemeHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemeHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                    child: Text(
+                                      'Logar'.toUpperCase(),
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () async {
+                                    await FirebaseAuth.instance
+                                        .signInWithEmailAndPassword(
+                                      email: emailController.text,
+                                      password: passwordController.text,
+                                    );
+                                    setState(() {});
+
+                                    DocumentSnapshot<Map<String, dynamic>>
+                                        userlogged = await FirebaseFirestore
+                                            .instance
+                                            .collection("users")
+                                            .doc(user?.uid)
+                                            .get();
+
+                                    if (user != null) {
+                                      if (userlogged.data()!['check']) {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomePageRevendedor()));
+                                      } else {
+                                        Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    HomePage()));
+                                      }
+                                    }
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                //child: Text('Don\'t have an account? Create'),
+                                child: Text.rich(TextSpan(children: [
+                                  TextSpan(text: "Não possui uma conta? "),
+                                  TextSpan(
+                                    text: 'Criar',
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    RegistrationPage()));
+                                      },
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).accentColor),
+                                  ),
+                                ])),
+                              ),
+                            ],
+                          )),
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
